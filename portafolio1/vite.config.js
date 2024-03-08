f7e381d2d165b949919bea49c860bbd4acc60a80
Edwin Rojas <enrojass04@gmail.com>
2024-03-08 16:24:34 -0500

    agregando actualización de ghpages

--- portafolio1/vite.config.js
+++ portafolio1/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://enrojass04.github.io/Reto-1-de-Programacion/'
 })
